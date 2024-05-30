f7771f22fbcebdbaffd3898b00a26bd33e37082a
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2024-05-30 11:39:03 +0200

    [+] Project Made

---
+++ Miner_Bob/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading;
+using System.ComponentModel; //Backgroundworker
+
+
+
+Console.Clear();
+
+Console.WriteLine("Sinterklaas... wie kent hem niet?!");
+
+BackgroundWorker thread = new BackgroundWorker();
+thread.DoWork += keypressThread;
+thread.RunWorkerAsync();
+
